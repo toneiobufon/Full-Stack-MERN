ee4a0256620cbc2e38c4a19e0419d4dea4cc556d
toneiojimz <ajimeneza77@gmail.com>
2021-03-12 12:23:50 -0800

    created SecondStep component to enteremail and password, also added a route on AppRouter to render the component

--- full-stack-mern/src/router/AppRouter.js
+++ full-stack-mern/src/router/AppRouter.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FirstStep from '../components/FirstStep.js';
 import Header from '../components/Header.js';
+import SecondStep from '../components/SecondStep.js';
 
-const AppRouter = () => {
+const AppRouter = () => (
 
 
-    return (
+    
         <BrowserRouter>
             <div className="container">
                 <Header />
                 <Switch>
                     <Route component={FirstStep} path="/" exact={true} />
+                    <Route component={SecondStep} path="/second" />
                 </Switch>
             </div>
         </BrowserRouter>
 
     );  
-};
 
 export default AppRouter;
