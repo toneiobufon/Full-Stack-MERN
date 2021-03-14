90c994ac601303600040634820316b0d00b2f8db
toneiojimz <ajimeneza77@gmail.com>
2021-03-13 16:06:35 -0800

    added props to render before component in the steps process

--- full-stack-mern/src/router/AppRouter.js
+++ full-stack-mern/src/router/AppRouter.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FirstStep from '../components/FirstStep.js';
 import Header from '../components/Header.js';
 import SecondStep from '../components/SecondStep.js';
 import ThirdStep from '../components/ThirdStep.js';
 
-const AppRouter = () => (
+const AppRouter = () => {
 
 
+    const [user, setUser] = useState({});
+
+    const updateUser = (data) => {
+        setUser((prevUser) => ({ ...prevUser, ...data }));
+    };
+
+    const resetUser = () => {
+        setUser({});
+    };
+
+    return(
     
         <BrowserRouter>
             <div className="container">
                 <Header />
                 <Switch>
-                    <Route component={FirstStep} path="/" exact={true} />
-                    <Route component={SecondStep} path="/second" />
-                    <Route component={ThirdStep} path= "/third"/>
+                    <Route
+                    render={(props) => (
+                        <FirstStep {...props} user={user} updateUser={updateUser} />
+                        )}
+                        path="/"
+                        exact={true} 
+                    />
+                    <Route
+                        render={(props) => (
+                            <SecondStep {...props} user={user} updateUser={updateUser} />
+                            )}
+                            path="/second" 
+                    />
+                    <Route
+                        render={(props) => (
+                            <ThirdStep {...props} user={user}  />
+                            )}
+                            path="/third"
+                    />
                 </Switch>
             </div>
         </BrowserRouter>
-
-    );  
+);
+};  
 
 export default AppRouter;
