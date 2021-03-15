6f190808d06acda60dab38beacae5dff2fc3e211
toneiojimz <ajimeneza77@gmail.com>
2021-03-15 13:53:09 -0700

    added userRouter as middleware for Express app inside server/index

--- full-stack-mern/server/index.js
+++ full-stack-mern/server/index.js
@@ -1,8 +1,13 @@
+
 const express = require('express');
+const userRouter = require('./routers/user');
 require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
+
+app.use(express.json());
+app.use(userRouter);
 
 app.get('/', (req, res) => {
   res.send('<h2>Nothing to see here, yet!</h2>');
